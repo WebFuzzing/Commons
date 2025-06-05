54dc9498bc8fc21794d4614f2248d4c846e2a475
arcuri82 <arcuri82@gmail.com>
2025-06-05 13:57:50 +0200

    added fault documentation

--- src/main/java/com/webfuzzing/commons/faults/DefinedFaultCategory.java
+++ src/main/java/com/webfuzzing/commons/faults/DefinedFaultCategory.java
@@ -12,12 +12,28 @@ public enum DefinedFaultCategory implements FaultCategory {
         code label are still up to discussion and re-arrangement...
      */
 
-    //1xx: HTTP
+    // 1xx: Base
+    // might keep other 2xx,...,8xx for other groups (eg, security, GraphQL, RPC)
+    // 9xx is reserved for custom codes
 
     HTTP_STATUS_500(100, "HTTP Status 500", "causes500_internalServerError",
-            "TODO"),
+            "The HTTP status code 500 represents a 'Server Error'." +
+                    " Typically, when there is crash in the business logic of the tested backend, like for example due to" +
+                    " a null-pointer exception, the server would not crash, but rather return a response with status code 500." +
+                    " Therefore, the presence of such a response 'might' indicate the presence of a fault in the backend." +
+                    " However, such code might also be used for other cases that have nothing to do with software faults." +
+                    " For example, if a request cannot be handled due to issue with the environment, e.g., databases and " +
+                    " communications with other APIs, a status code 500 could be sent." +
+                    " As such, although there is high chances that a 500 status code might point to the presence of a" +
+                    " software fault in the tested application, they still need to be manually checked due to possible 'false-positive'."),
+
     SCHEMA_INVALID_RESPONSE(101, "Received A Response From API That Is Not Valid According To Its Schema", "returnsSchemaInvalidResponse",
-            "TODO"),
+            "A schema, like for example OpenAPI for REST, defines the structures not only of the inputs but" +
+                    " also the outputs of the API." +
+                    " If what returned by an API is not conforming to its schema, then it is a clear fault." +
+                    " However, whether the fault is in the API (i.e., it does not conform to the schema) or in the schema" +
+                    " itself (i.e., it is underspecified, or having mistakes) is something that cannot be known for" +
+                    " sure without debugging the issue."),
 
 
     ;
